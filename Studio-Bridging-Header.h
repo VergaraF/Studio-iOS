bcd185485e60f928a4c07d968fa9d572ae839517
Fabian Vergara <vergarf@gmail.com>
2016-12-11 14:35:55 -0500

    Added header-file

---
+++ Studio-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  Studio-Bridging-Header.h
+//  Studio
+//
+//  Created by Fabian Vergara on 2016-07-22.
+//  Copyright © 2016 fvergara. All rights reserved.
+//
+
+#ifndef Studio_Bridging_Header_h
+#define Studio_Bridging_Header_h
+
+
+#endif /* Studio_Bridging_Header_h */
+
+#import <JSQMessagesViewController/JSQMessages.h> 
