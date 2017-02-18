1d8ea63c31459df571c091d131de9d7336bf6fd6
Fabian Vergara <vergarf@gmail.com>
2017-02-18 18:47:56 -0500

    Finishing up registration process

--- UserStatusViewController.swift
+++ UserStatusViewController.swift
@@ -16,8 +16,29 @@ import SCLAlertView
 
 class UserStatusViewController: UIViewController{
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         
         // Handle the text field’s user input through delegate callbacks.
+    }
+    
+    //tag 0 for tutor. Tag 1 for student
+    
+    @IBAction func isUserTutorOrStudentAction(_ sender: Any) {
+        switch ( (sender as! UIButton).tag){
+        case 0:
+            print("tutor")
+            UserDefaults.standard.set(true, forKey: "isTutor")
+            //go to tutor panel control
+//            break
+        case 1:
+            print("student")
+            UserDefaults.standard.set(false, forKey: "isTutor")
+            self.performSegue(withIdentifier: "goToSubjects", sender: self)
+
+//            break
+        default:
+            print("none")
+        }
     }
 }
