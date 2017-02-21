dc227268818302ccb2912a6a085eb3d32cd7c4cf
Fabian Vergara <vergarf@gmail.com>
2017-02-21 02:13:24 -0500

    A new class, new view, implementation of nav bar

---
+++ Studio/StudentSubjectsController.swift
@@ -0,0 +1,30 @@
+//
+//  ClassesController.swift
+//  Studio
+//
+//  Created by Fabian Vergara on 2017-02-21.
+//  Copyright © 2017 Studio. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import FacebookCore
+import FacebookLogin
+import FBSDKCoreKit
+import Firebase
+import SCLAlertView
+
+class StudentSubjectsController: UIViewController{
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        
+        // Handle the text field’s user input through delegate callbacks.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        navigationItem.title = "One"
+    }
+    
+    
+}
