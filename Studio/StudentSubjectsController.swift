e908aee4b45a8230d04960f5883924f751dfbd79
Fabian Vergara <vergarf@gmail.com>
2017-02-21 19:13:28 -0500

    Some of the tableview functionality implemented

--- Studio/StudentSubjectsController.swift
+++ Studio/StudentSubjectsController.swift
@@ -14,10 +14,18 @@ import FBSDKCoreKit
 import Firebase
 import SCLAlertView
 
-class StudentSubjectsController: UIViewController{
+class StudentSubjectsController: UIViewController, UITableViewDelegate, UITableViewDataSource{
+
+    
+    var counter = 0
+    @IBOutlet var tableView: UITableView!
+    
     override func viewDidLoad() {
         
         super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
         
         // Handle the text field’s user input through delegate callbacks.
     }
@@ -25,6 +33,28 @@ class StudentSubjectsController: UIViewController{
     override func viewWillAppear(_ animated: Bool) {
         navigationItem.title = "Subjects"
         
+    }
+
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+        let cell = UITableViewCell(style: UITableViewCellStyle.default,reuseIdentifier: "SubjectCell")
+
+       
+        if indexPath.row % 2 != 0{
+            cell.backgroundColor = UIColor.cyan
+
+        }
+
+    
+        //cell.textLabel?.text = String(timesTableValue * indexPath.row)
+        
+        return cell
+    }
+    
+    //TODO
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 5
     }
     
     
